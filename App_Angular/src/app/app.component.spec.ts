91ad3492daac1e0c51943acb09f64cc2bbe12382
Selepe Sello <sbkskhalo.kq@gmail.com>
2022-09-10 08:06:51 +0200

    Second Commit for The Components

--- App_Angular/src/app/app.component.spec.ts
+++ App_Angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'App_Angular'`, () => {
+  it(`should have as title 'Straw-Cat-App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('App_Angular');
+    expect(app.title).toEqual('Straw-Cat-App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('App_Angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Straw-Cat-App app is running!');
   });
 });
